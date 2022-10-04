acf5fbd0477721da87a8167b974bb947b4747c95
lokasuchithrareddy <lokasuchithrareddy@gmail.com>
2022-10-04 21:13:50 +0530

    initial commit

---
+++ DB/db.sql
@@ -0,0 +1,71 @@
+/*
+SQLyog Community v11.52 (32 bit)
+MySQL - 5.5.30 : Database - securersa
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`securersa` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `securersa`;
+
+/*Table structure for table `cloud` */
+
+DROP TABLE IF EXISTS `cloud`;
+
+CREATE TABLE `cloud` (
+  `fid` varchar(100) DEFAULT NULL,
+  `filenm` varchar(100) DEFAULT NULL,
+  `filedata` longblob,
+  `uid` varchar(100) DEFAULT NULL,
+  `aeskey` blob,
+  `owner` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `owner` */
+
+DROP TABLE IF EXISTS `owner`;
+
+CREATE TABLE `owner` (
+  `name` varchar(100) DEFAULT NULL,
+  `uid` varchar(500) DEFAULT NULL,
+  `pwd` varchar(500) DEFAULT NULL,
+  `email` varchar(500) DEFAULT NULL,
+  `mno` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `temp` */
+
+DROP TABLE IF EXISTS `temp`;
+
+CREATE TABLE `temp` (
+  `filenm` varchar(500) DEFAULT NULL,
+  `filedata` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `name` varchar(100) DEFAULT NULL,
+  `uid` varchar(100) DEFAULT NULL,
+  `pwd` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mno` varchar(100) DEFAULT NULL,
+  `pk` varchar(1000) DEFAULT NULL,
+  `sk` varchar(1000) DEFAULT NULL,
+  `n` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
